7b7ad3a37356ee607e031dd2c4d854e7f312276f
Kağan Yarımca <kyarimca48@gmail.com>
2023-10-18 15:15:03 +0300

    updating project

--- src/Book.java
+++ src/Book.java
@@ -4,9 +4,47 @@ public class Book implements Comparable<Book>   {
     private String author;
     private  String releaseDate;
 
+    public Book(String name, int page, String author, String releaseDate) {
+        this.name = name;
+        this.page = page;
+        this.author = author;
+        this.releaseDate = releaseDate;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getPage() {
+        return page;
+    }
 
+    public void setPage(int page) {
+        this.page = page;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public int compareTo(Book book){
+        return getName().compareTo((book.name));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
+    TreeSet<Book> books =new TreeSet<>();
+
+    Book b1= new Book("Dava",224,"Franz Kafka","1924");
+    Book b2= new Book("Bin Dokuz Seksen Dört",352,"George Orwell","1949");
+    Book b3 = new Book("İnsanlık Durumu",320, "André Malraux","1933");
+    Book b4= new Book("Küçük Prens",112,"Antoine de Saint-Exupery","1933");
+
+
 
 
     }
