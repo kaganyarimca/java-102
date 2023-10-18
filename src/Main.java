9bb76e2e969dc854f56e8afa526e5b7f0cb7c0fa
Kağan Yarımca <kyarimca48@gmail.com>
2023-10-18 15:40:35 +0300

    last updating project

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,39 @@ public class Main {
     Book b3 = new Book("İnsanlık Durumu",320, "André Malraux","1933");
     Book b4= new Book("Küçük Prens",112,"Antoine de Saint-Exupery","1933");
 
+    books.add(b1);
+    books.add(b2);
+    books.add(b3);
+    books.add(b4);
+
+for(Book book : books){
+    System.out.println("Kitap İsmi: "+ book.getName()
+            + ", Sayfa Sayısı: "+ book.getPage()
+            + ", Yazar İsmi " + book.getAuthor()
+            + ", Yayınlanma Tarihi: " + book.getReleaseDate()  );
+    }
+        System.out.println("************************* \n");
+
+    TreeSet<Book> books2 = new TreeSet<>(new Comparator<Book>() {
+        @Override
+        public int compare(Book o1, Book o2) {
+            return o1.getPage()- o2.getPage();
+        }
+    } );
+
+    books2.add(b1);
+    books2.add(b2);
+    books2.add(b3);
+    books2.add(b4);
+        System.out.println("Sayfa sayısını göre sıralanmıştır.\n");
+    for (Book book : books2){
+        System.out.println("Kitap İsmi: "+ book.getName()
+                + ", Sayfa Sayısı: "+ book.getPage()
+                + ", Yazar İsmi " + book.getAuthor()
+                + ", Yayınlanma Tarihi: " + book.getReleaseDate()  );
+
+    }
+
 
 
 
